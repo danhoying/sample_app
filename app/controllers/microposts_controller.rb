2fffec63305cceb504a075e11830a8f5149a5c9c
Dan Hoying <danhoying@gmail.com>
2016-04-08 07:43:09 -0700

    Add Micropost form and update error messages partial

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -7,7 +7,7 @@ class MicropostsController < ApplicationController
       flash[:success] = "Micropost created!"
       redirect_to root_url
     else
-      render 'staticpages/home'
+      render 'static_pages/home'
     end
   end
 
